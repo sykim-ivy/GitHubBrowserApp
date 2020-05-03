8143e629116b5682ea03815919510acd9e958acf
sykim <skim.ivy@gmail.com>
2020-05-04 02:25:06 +0900

    AAC ViewModel 적용 설명 주석 추가중

--- app/src/main/java/com/omjoonkim/app/githubBrowserApp/ui/search/SearchActivity.kt
+++ app/src/main/java/com/omjoonkim/app/githubBrowserApp/ui/search/SearchActivity.kt
@@ -18,14 +18,17 @@ class SearchActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // [syk][DataBiding]
         val binding = DataBindingUtil.setContentView<ActivitySearchBinding>(this, R.layout.activity_search)
-        binding.setLifecycleOwner(this)
+        binding.lifecycleOwner = this // [syk] lifecycleOwner를 바인딩한 객체에게 줌 --이를 통해-->> 데이터 변화에 따라 View갱신 & View가 종료 시 해당 stream도 종료됨
+
         actionbarInit(binding.toolbar, isEnableNavi = false)
 
-        val viewModel = getViewModel<SearchViewModel>()
+        val viewModel = getViewModel<SearchViewModel>() // [syk] Koin을 통해 ViewModel을 얻어옮
         binding.viewModel = viewModel
 
-        viewModel.output.goResultActivity()
+        // [syk] Router 부분 : 검색시 이름값을 전달받아 ResultActivity로 이동
+        viewModel.output.goResultActivity() // [syk] LiveData를 Observe하므로 ViewModel이 clear || View가 종료 시 해당 stream도 종료됨
             .observe {
                 keyboardController.hideSoftInputFromWindow(binding.editText.windowToken, 0)
                 startActivity(
